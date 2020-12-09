4b384421048bc63a179339bd965422d0840f03a6
cury009 <63730861+cury009@users.noreply.github.com>
2020-12-09 21:47:17 +0100

    Agregar archivos de proyecto.

---
+++ VT-01RaulSalva/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VT_01RaulSalva.Pages;
+
+namespace VT_01RaulSalva
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static Frame myNavigationFrame;
+        public UsersHandler usersHandler;
+        public MainWindow()
+        {
+            InitializeComponent();
+            myNavigationFrame = myFrame;
+            usersHandler = new UsersHandler();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myNavigationFrame.NavigationService.Navigate(new MainPage());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            myNavigationFrame.NavigationService.Navigate(new NewOrModifyUserPage("Nuevo usuario", usersHandler));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            myNavigationFrame.NavigationService.Navigate(new ShowUserPage(usersHandler));
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
